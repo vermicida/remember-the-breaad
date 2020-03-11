a7efb51199e77f0d92f495b4746b1a1927cd0023
Diego Herrera <vermicida@gmail.com>
2020-03-11 10:56:11 +0100

    Fix Secrets Manager client instantiation

--- config.py
+++ config.py
@@ -6,13 +6,17 @@ import os
 import sys
 from botocore.exceptions import ClientError
 
-SECRETS_MANAGER_NAME = 'rtb-db-secret'
+REGION_NAME = 'eu-west-1'
+SECRET_NAME = 'rtb-db-secret'
 
 
 def get_secret(secret_name):
 
     session = boto3.session.Session()
-    client = session.client(service_name='secretsmanager')
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=REGION_NAME
+    )
 
     try:
         response = client.get_secret_value(SecretId=secret_name)
@@ -30,5 +34,5 @@ def get_secret(secret_name):
 class Config:
     ENV = os.environ.get('FLASK_ENV') or 'development'
     DEBUG = os.environ.get('FLASK_DEBUG') or False
-    SQLALCHEMY_DATABASE_URI = 'mysql://{username}:{password}@{host}/{db}'.format(**get_secret(SECRETS_MANAGER_NAME))
+    SQLALCHEMY_DATABASE_URI = 'mysql://{username}:{password}@{host}/{db}'.format(**get_secret(SECRET_NAME))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
